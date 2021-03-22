5cd27fb0b30aacf3ff1b1ed05775eeea08a843d2
liufulong <liufulong@douyu.tv>
2021-03-22 13:49:42 +0800

    add log

--- dida-server/app/service/iep.js
+++ dida-server/app/service/iep.js
@@ -5,11 +5,14 @@ const { Service } = require('egg');
 
 class IEPService extends Service {
   async findUser(userData) {
+    ctx.logger.info('this.ctx.model:', this.ctx.model);
+    ctx.logger.info('this.ctx.model.iEP:', this.ctx.model.iEP);
+    ctx.logger.info('this.ctx.model.iEP.find:', this.ctx.model.iEP.find);
     const user = await this.ctx.model.iEP.find({ unionId: userData.unionId });
     return user;
   }
   async addUser(data = {}) {
-    const newUser = new this.app.model.IEP({
+    const newUser = new this.app.model.iEP({
       ...data,
     });
     await newUser.save();
